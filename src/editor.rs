97e7c581279a508dd5c114d267f1341fdb5e7563
Sudarshan <sudar.theone@gmail.com>
2018-02-06 15:25:11 +0530

    src: modularized editor and refresh when close

--- src/editor.rs
+++ src/editor.rs
@@ -1,24 +1,40 @@
-use std::io;
 use std::io::prelude::*;
 use std::error::Error;
+use std::{io, thread, time};
 
-fn read_key() -> Result<u8, Box<Error>> {
-    let mut c = [0];
-    let stdin = io::stdin();
-    stdin.lock().read(&mut c)?;
-    Ok(c[0])
-}
+pub struct Editor {}
 
-pub fn process_key_press() -> Result<bool, Box<Error>> {
-    let c = read_key()?;
-    match c {
-        24 => return Ok(false),
-        _ => println!("\r{} : {}", c, c as char),
+impl Editor {
+    pub fn new() -> Editor {
+        Editor {}
     }
-    Ok(true)
+
+    fn read_key(&self) -> Result<u8, Box<Error>> {
+        let mut c = [0];
+        let stdin = io::stdin();
+        stdin.lock().read(&mut c)?;
+        Ok(c[0])
+    }
+
+    pub fn process_key_press(&self) -> Result<bool, Box<Error>> {
+        let c = self.read_key()?;
+        match c {
+            24 => return Ok(false),
+            _ => println!("\r{} : {}", c, c as char),
+        }
+        Ok(true)
+    }
+
+    pub fn refresh_screen(&self) {
+        print!("{}[2J", 27 as char);
+        print!("{}[H", 27 as char);
+    }
 }
 
-pub fn refresh_screen() {
-    print!("{}[2J", 27 as char);
-    print!("{}[H", 27 as char);
+impl Drop for Editor {
+    fn drop(&mut self) {
+        println!("exiting adamantium...");
+        thread::sleep(time::Duration::from_millis(1000));
+        Editor::refresh_screen(&self);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,15 @@ extern crate adamantium;
 extern crate termios;
 
 use adamantium::term::Term;
-use adamantium::editor;
+use adamantium::editor::Editor;
 
 fn main() {
     let mut t = Term::new().unwrap();
+    let editor = Editor::new();
     t.enable_raw_mode().unwrap();
     loop {
-        editor::refresh_screen();
-        let ok = editor::process_key_press().unwrap();
+        editor.refresh_screen();
+        let ok = editor.process_key_press().unwrap();
         if !ok {
             break;
         }
